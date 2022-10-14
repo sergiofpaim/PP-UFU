18e9e79a8a5e351ea8ae5cdb7ae18146f8931080
Sérgio Filho Garcia Paim <sergio.filho@outlook.com>
2022-10-14 11:53:03 -0300

    Criada pasta

---
+++ Lab02/exercise15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    float g, r;
+    printf("Digite um angulo em radianos: ");
+    scanf("%f", &r);
+    g = r * 180 / 3.141592;
+    printf("%f", g);
+
+    return 0;
+}
