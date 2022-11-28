accca84d77ec28481ef93885d1c8d2f344101f9d
Sérgio Filho Garcia Paim <sergio.filho@outlook.com>
2022-11-28 09:42:35 -0300

    Exercício 7 adicionado

--- Lab06/exercise7.c
+++ Lab06/exercise7.c
@@ -2,20 +2,31 @@
 
 void main()
 {
-    int value;
-    int vector[10];
+    int value, sum = 0, total = 0;
+    int vector[6];
 
-    printf("Digite 10 valores sem repeti-los no vetor\n");
-
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 6; i++)
     {
-        for (int j = 0; j < i; j++)
-            if (vector[i] != vector[j])
-            {
-                scanf("%d", &value);
-                vector[i] = value;
-            }
-            else
-                printf("Digite outro numero");
+        printf("Digite um valor para a %da posicao: ", (i + 1));
+        scanf("%d", &value);
+        vector[i] = value;
     }
+
+    printf("Os numeros pares sao\n");
+    for (int i = 0; i < 6; i++)
+        if ((vector[i] % 2) == 0)
+        {
+            printf("%d\n", vector[i]);
+            sum += vector[i];
+        }
+    printf("A soma dos numeros pares eh: %d\n\n", sum);
+
+    printf("Os numeros impares sao\n");
+    for (int i = 0; i < 6; i++)
+        if ((vector[i] % 2) != 0)
+        {
+            printf("%d\n", vector[i]);
+            total++;
+        }
+    printf("A quantidade de numeros impares digitados eh: %d\n", total);
 }
