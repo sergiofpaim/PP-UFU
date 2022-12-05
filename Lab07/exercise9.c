b5d177653bb0c1f335655f5956e10ba2f4e011e2
Sérgio Filho Garcia Paim <sergio.filho@outlook.com>
2022-12-05 07:27:27 -0300

    Exercício 9 adicionado

--- Lab07/exercise9.c
+++ Lab07/exercise9.c
@@ -2,20 +2,16 @@
 #include <string.h>
 void main()
 {
-    char str[5][2] = {"Fusca", "Gol", "Vectra", "Fusion", "Renegade"}, {"5", "10", "8", "7", "12"};
+    char str[8][8] = {"Fusca", "Gol", "Vectra", "Fusion", "Renegade"};
+    int arr[5] = {5, 10, 8, 7, 12}, temp = 0;
 
     printf("\n\nO carro mais economico eh: ");
 
-    for (int i = 0; i < strlen(str); i++)
-        if (str[i] == l1)
-            str[i] = l2;
+    for (int i = 0; i < 5; ++i)
+        if (arr[temp] < arr[i])
+            temp = i;
 
-    printf("\n\nDigite uma para substituir a letra previamente escrita: ");
-    scanf(" %c", &l2);
-
-    for (int i = 0; i < strlen(str); i++)
-        if (str[i] == l1)
-            str[i] = l2;
-
-    printf("%s", str);
+    for (int i = 0; i < 5; i++)
+        if (i == temp)
+            printf("%s", str[i]);
 }
