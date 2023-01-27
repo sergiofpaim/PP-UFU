b1beea62aed4ae1aa4b199e0cf33a2bd592bdd76
Sérgio Filho Garcia Paim <sergio.filho@outlook.com>
2023-01-27 15:53:46 -0300

    Randomização funcionado

--- TCD/cardGame.c
+++ TCD/cardGame.c
@@ -3,13 +3,9 @@
 #include <string.h>
 #include <time.h>
 
-struct fullDeck
-{
-    int cardsV[40];
-    char cardsN[40][20];
-};
+int fillDeckValue(int num);
 
-struct myDeck
+struct playerDeck
 {
     int cardsV[3];
     char cardsN[3][20];
@@ -20,6 +16,8 @@ void main()
     srand(time(NULL));
 
     int roundCount = 0, handState = 0, playerWins = 0, iaWins = 0;
+
+    struct playerDeck player[2];
 
     char deckName[40][20] = {"Quatro de Ouros", "Quatro de Espada", "Quatro de Copas",
                              "Cinco de Ouros", "Cinco de Espada", "Cinco de Copas", "Cinco de Paus",
@@ -33,19 +31,43 @@ void main()
                              "Tres de Ouros", "Tres de Espada", "Tres de Copas", "Tres de Paus",
                              "Sete de Ouros", "Espadilha", "Sete de Copas", "Zap"};
 
-    int deckValue[40] = {1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7,
-                         8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 12, 13, 14};
-
-    struct myDeck player[2];
-
     for (int i = 0; i < 2; i++)
         for (int j = 0; j < 3; j++)
         {
+            int deck[6] = {-1, -1, -1, -1, -1, -1};
             int tempNumber = rand() % 40;
-
-            player[i].cardsV[j] = deckValue[tempNumber];
-            strcpy(player[i].cardsN[j], deckName[tempNumber]);
+            for (int k = 0; k < 6; k++)
+                for (int l = 0; l < k; l++)
+                {
+                    if (tempNumber == deck[l])
+                    {
+                        tempNumber = rand() % 40;
+                        k--;
+                    }
+                    else
+                    {
+                        deck[k] = tempNumber;
+                        player[i].cardsV[j] = fillDeckValue(tempNumber);
+                        strcpy(player[i].cardsN[j], deckName[tempNumber]);
+                    }
+                }
         }
 
-    printf("%s", player[1].cardsN[1]);
+    printf("Suas cartas sao\n\n");
+
+    for (int i = 0; i < 3; i++)
+        printf("%s   ", player[0].cardsN[i]);
+
+    printf("\n\nAs cartas do inimigo sao\n\n");
+
+    for (int i = 0; i < 3; i++)
+        printf("%s   ", player[1].cardsN[i]);
+}
+
+int fillDeckValue(int num)
+{
+    int deckValue[40] = {1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7,
+                         8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 12, 13, 14};
+
+    return deckValue[num];
 }
